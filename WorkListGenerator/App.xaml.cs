aa1c577210bd3d1f5bbc21939ad5465aeff319ac
Aleksey Zabrodin <rediskal50@gmail.com>
2022-01-26 18:55:29 +0300

    Add connector to DataBAse

--- WorkListGenerator/App.xaml.cs
+++ WorkListGenerator/App.xaml.cs
@@ -13,5 +13,13 @@ namespace WorkListGenerator
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainWindow = new MainWindow();
+            mainWindow.DataContext = new MainViewModel();
+            mainWindow.Show();
+        }
     }
 }
