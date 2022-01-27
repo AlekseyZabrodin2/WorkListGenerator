9eb79d45e029e6293513f37d63c266e9be32b28e
Aleksey Zabrodin <rediskal50@gmail.com>
2022-01-27 10:10:55 +0300

    Merge remote-tracking branch 'origin/master' Add Files

---
+++ WorkListGenerator/Model/IGeneratorRule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkListGenerator.Model
+{
+    public interface IGeneratorRule<T>
+    {
+        T Generate();
+    }
+
+}
